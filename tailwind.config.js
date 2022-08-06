9412296b326c8d37c3def4f01ae3729ff81927a5
cheeto <cheeto.tw0268@gmail.com>
2022-08-06 23:16:51 +0800

    feat: 🎸 tailwind config 基礎配置

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'jit',
+  content: [],
+  theme: {
+    fontFamily: {
+      sans: ['Noto Sans TC', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#e2b26e',
+        'mtv-primary': {
+          DEFAULT: '#ffb715'
+        },
+      },
+      backgroundColor: theme => ({
+        ...theme('colors')
+      }),
+      borderColor: theme => ({
+        ...theme('colors')
+      })
+    }
+  },
+  variants: {
+  }
+};
